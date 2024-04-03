8c8ba6a50fa088661ea8b647b61ecfa896bde12e
R3flex212 <mateineamtu8@gmail.com>
2024-04-03 16:57:42 +0300

    Added images and results

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 let questionDict = {
